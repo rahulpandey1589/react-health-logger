111deea577b43804e3ff29c81d1c13c82221f534
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-08-31 21:03:08 +0530

    Added controls in login component

--- src/LoginComponent/login-component.js
+++ src/LoginComponent/login-component.js
@@ -1,7 +1,36 @@
 const LoginComponent = () => {
-  return <>
-     <h1>Welcome to Login Component</h1>
-  </>;
+  return (
+    <>
+      <form>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-4">
+              <label name="username">UserName</label>
+              <input
+                name="username"
+                type="text"
+                id="username"
+                className="form-control"
+                placeholder="Please Enter UserName"
+              />
+              <br />
+              <label name="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Please Enter UserName"
+              />
+              <br />
+              <button type="submit" className="btn btn-primary">Login</button> &nbsp;
+              <button type="button" className="btn btn-primary">Forgot Password</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default LoginComponent;
