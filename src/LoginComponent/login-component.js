bef13c6e8e704534459352916346b3bf70bdc3b9
Rahul Pandey <rahulpandey862@gmail.com>
2021-09-01 11:09:19 +0530

    Added Change Handler in login.component.js

--- src/LoginComponent/login-component.js
+++ src/LoginComponent/login-component.js
@@ -1,15 +1,36 @@
+import { useState } from "react";
+
 const LoginComponent = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const nameChangeHandler = ($event) => setUserName($event.target.value);
+  const passwordChangeHandler = ($event) => setPassword($event.target.value);
+
+  const submitFormHandler = ($event) => {
+    $event.preventDefault();
+    if (username === "rahulpandey862@gmail.com" && password === "rahul@123") {
+      clearState();
+    }
+  };
+
+  const clearState = () => {
+    setUserName("");
+    setPassword("");
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div className="container-fluid">
           <div className="row">
-            <div className="col-md-4">
+            <div className="col-md-8 offset-2">
               <label name="username">UserName</label>
               <input
                 name="username"
-                type="text"
-                id="username"
+                type="email"
+                value={username}
+                onChange={nameChangeHandler}
                 className="form-control"
                 placeholder="Please Enter UserName"
               />
@@ -18,13 +39,23 @@ const LoginComponent = () => {
               <input
                 name="password"
                 type="password"
-                id="password"
+                value={password}
+                minLength="8"
+                maxLength="16"
+                onChange={passwordChangeHandler}
                 className="form-control"
-                placeholder="Please Enter UserName"
+                placeholder="Please Enter Password"
               />
-              <br />
-              <button type="submit" className="btn btn-primary">Login</button> &nbsp;
-              <button type="button" className="btn btn-primary">Forgot Password</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4 offset-4">
+              <button type="submit" className="btn btn-success">
+                Login
+              </button>
+              <button type="button" className="btn btn-success">
+                Register
+              </button>
             </div>
           </div>
         </div>
