2943f1916e88bdedf61c5a141dd0630ba7bdce0c
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-09-02 14:19:02 +0530

    Validation for login.js

--- src/LoginComponent/login-component.js
+++ src/LoginComponent/login-component.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
+import "./login-component.css";
 
 const LoginComponent = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-
-  let errors = {
-    username: "",
-    password: "",
-  };
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -15,9 +13,11 @@ const LoginComponent = () => {
 
     switch (name) {
       case "username":
+        validateUserName(value);
         setUserName(value);
         break;
       case "password":
+        validateUserPassword(value);
         setPassword(value);
         break;
       default:
@@ -25,17 +25,27 @@ const LoginComponent = () => {
     }
   };
 
-  const validate = () =>{
-    let userNameError="";
-
-    if(!username.includes('@')){
-      userNameError='Invalid email';
+  const validateUserName = (value) => {
+    if (!value.includes("@")) {
+      setEmailError("The entered email address is not in correct format.");
+    } else {
+      setEmailError("");
     }
-  }
+  };
+
+  const validateUserPassword = (password) => {
+    if (password.length < 5 || password.length > 15) {
+      setPasswordError(
+        "Password should be greater than 5 character and should be less than 15."
+      );
+    } else {
+      setPasswordError("");
+    }
+  };
 
   const handleFormSubmit = ($event) => {
     $event.preventDefault();
-    
+    clearState();
   };
 
   const clearState = () => {
@@ -46,39 +56,42 @@ const LoginComponent = () => {
   return (
     <form onSubmit={handleFormSubmit}>
       <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-8 offset-2">
-            <label name="username">UserName</label>
-            <input
-              name="username"
-              type="text"
-              value={username}
-              onChange={handleChange}
-              className="form-control"
-              placeholder="Please Enter UserName"
-            />
-            {errors.username.length > 0 && <span>{errors.username}</span>}
-            <br />
-            <label name="password">Password</label>
-            <input
-              name="password"
-              type="password"
-              value={password}
-              onChange={handleChange}
-              className="form-control"
-              placeholder="Please Enter Password"
-            />
-            {errors.password.length > 0 && <span>{errors.password}</span>}
-          </div>
+        <div className="row col-md-4 offset-2">
+          <label name="username">UserName</label>
+          <input
+            name="username"
+            type="text"
+            value={username}
+            onChange={handleChange}
+            className="form-control"
+            placeholder="Please Enter UserName"
+          />
+          {emailError.length > 0 && (
+            <span className="span-error">{emailError}</span>
+          )}
+          <br />
+          <label name="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={handleChange}
+            className="form-control"
+            placeholder="Please Enter Password"
+          />
+          {passwordError.length > 0 && (
+            <span className="span-error">{passwordError}</span>
+          )}
+        </div>
+        <br />
+        <div className="row col-md-4 offset-2">
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
         </div>
         <div className="row">
           <div className="col-md-4 offset-4">
-            <button type="submit" className="btn btn-success">
-              Login
-            </button>
-            <button type="button" className="btn btn-success">
-              Register
-            </button>
+            <a href="">Forgot Password</a>
           </div>
         </div>
       </div>
--- src/LoginComponent/register-component.js
+++ src/LoginComponent/register-component.js
@@ -1,9 +1,31 @@
 const RegisterComponent = () => {
   return (
-    <div>
-      <span>
-        Root Level URL : <b>{process.env.REACT_APP_API_URL}</b>
-      </span>
+    <div className="container-fluid">
+      <form>
+        <div className="row col-md-4 offset-2">
+          <label for="username">UserName</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="Please Enter UserName"
+            className="form-control"
+          />
+          <br />
+          <label for="password">Password</label>
+          <input
+            for="password"
+            type="password"
+            placeholder="Please Enter Password"
+            className="form-control"
+          />
+        </div>
+        <br />
+        <div className="row col-md-4 offset-2">
+           <button className="btn btn-primary">
+             Register
+           </button>
+        </div>
+      </form>
     </div>
   );
 };
