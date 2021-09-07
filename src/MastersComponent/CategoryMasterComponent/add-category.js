496492425833a146045b4a9be967285ca2140fa6
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-09-07 19:54:12 +0530

    Added category description master

--- src/MastersComponent/CategoryMasterComponent/add-category.js
+++ src/MastersComponent/CategoryMasterComponent/add-category.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ButtonComponent from "../../SharedComponent/button-component";
 import firebase from "../../firebase";
+import CategoryListComponent from "./list-category";
 
 const AddCategoryComponent = () => {
   const [category, setCategory] = useState("");
@@ -21,12 +22,12 @@ const AddCategoryComponent = () => {
 
   return (
     <>
-      <div className="container-fluid">
+      <div className="container">
         <div className="row">
-          <div className="col-md-2">
+          <div className="col-md-3">
             <label for="category">Category Name</label>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-5">
             <input
               className="form-control"
               type="text"
@@ -37,13 +38,16 @@ const AddCategoryComponent = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-4">
+          <div className="col-md-4 offset-6">
             <ButtonComponent
               label="Add Category"
               className="btn btn-success"
               onButtonClick={addCategory}
             ></ButtonComponent>
           </div>
+        </div>
+        <div className="row">
+          <CategoryListComponent></CategoryListComponent>
         </div>
       </div>
     </>
---
+++ src/MastersComponent/CategoryMasterComponent/list-category.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import firebase from "../../firebase";
+
+const CategoryListComponent = () => {
+  const categoryMasterDbRef = firebase.ref("Category");
+  const [categoryMaster, setCategoryMaster] = useState([]);
+
+  useEffect(() => {
+    loadCategory();
+  }, []);
+
+  const loadCategory = () => {
+    categoryMasterDbRef.on("value", (snapshot) => {
+      const response = snapshot.val();
+      const masterList = [];
+      for (let id in response) {
+        masterList.push({ id, ...response[id] });
+      }
+      setCategoryMaster(masterList);
+    });
+  };
+
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Category Name</th>
+            <th>Description</th>
+            <th>IsActive</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categoryMaster !== undefined &&
+            categoryMaster.map((item) => <tr>
+                <td>{item.CategoryName}</td>
+                <td>{item.Description}</td>
+                <td>{item.IsActive == true ? "True" : "False"}</td>
+            </tr>)}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default CategoryListComponent;
