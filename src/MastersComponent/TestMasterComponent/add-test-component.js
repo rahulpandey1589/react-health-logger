dc0868b25ec66e3fc92689635c64daf814851cb8
Rahul Pandey <rahul.pandey02@nagarro.com>
2021-09-28 19:11:56 +0530

    Changed button-component from functional component to class based component,Add useCallback in Test Master to prevent re-rendering of Button component

--- src/MastersComponent/TestMasterComponent/add-test-component.js
+++ src/MastersComponent/TestMasterComponent/add-test-component.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
+
 import firebase from "../../firebase";
 import ButtonComponent from "../../SharedComponent/button-component";
 import CategoryDropdownComponent from "../CategoryMasterComponent/category-dropdown-component";
@@ -35,12 +36,12 @@ const AddTestComponent = () => {
     }
   };
 
-  const addHandler = () => {
+  const addHandler = useCallback(() => {
     const testMasterRef = firebase.ref("TestMaster");
     let data = new TestMasterModel(title, description, category, price);
     console.log(data);
     testMasterRef.push(data);
-  };
+  }, []);
 
   const categoryChangeHandler = (data) => {
     setCategory(data);
@@ -48,60 +49,59 @@ const AddTestComponent = () => {
 
   return (
     <>
-
-        <div className="row">
-          <div className="col-4">
-            <label htmlFor="category">Category</label>
-            <CategoryDropdownComponent
-              onChange={categoryChangeHandler}
-            ></CategoryDropdownComponent>
-          </div>
-          <div className="col-4">
-            <label htmlFor="title">Title</label>
-            <input
-              className="form-control"
-              name="title"
-              type="text"
-              value={title}
-              onChange={onChangeHandler}
-              placeholder="Title"
-            ></input>
-          </div>
+      <div className="row">
+        <div className="col-4">
+          <label htmlFor="category">Category</label>
+          <CategoryDropdownComponent
+            onChange={categoryChangeHandler}
+          ></CategoryDropdownComponent>
         </div>
-        <div className="row">
-          <div className="col-4">
-            <label htmlFor="description">Description</label>
-            <input
-              className="form-control"
-              type="text"
-              name="description"
-              value={description}
-              onChange={onChangeHandler}
-              placeholder="Please enter description"
-            ></input>
-          </div>
-          <div className="col-4">
-            <label htmlFor="title">Price</label>
-            <input
-              className="form-control"
-              type="number"
-              name="price"
-              value={price}
-              onChange={onChangeHandler}
-              placeholder="Please enter price"
-            ></input>
-          </div>
+        <div className="col-4">
+          <label htmlFor="title">Title</label>
+          <input
+            className="form-control"
+            name="title"
+            type="text"
+            value={title}
+            onChange={onChangeHandler}
+            placeholder="Title"
+          ></input>
         </div>
-        <br/>
-        <div className="row">
-          <div className="col">
-            <ButtonComponent
-              label="Add New"
-              className="btn btn-success"
-              onButtonClick={addHandler}
-            ></ButtonComponent>
-          </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <label htmlFor="description">Description</label>
+          <input
+            className="form-control"
+            type="text"
+            name="description"
+            value={description}
+            onChange={onChangeHandler}
+            placeholder="Please enter description"
+          ></input>
         </div>
+        <div className="col-4">
+          <label htmlFor="title">Price</label>
+          <input
+            className="form-control"
+            type="number"
+            name="price"
+            value={price}
+            onChange={onChangeHandler}
+            placeholder="Please enter price"
+          ></input>
+        </div>
+      </div>
+      <br />
+      <div className="row">
+        <div className="col">
+          <ButtonComponent
+            label="Add New"
+            className="btn btn-success"
+            onButtonClick={addHandler}
+          ></ButtonComponent>
+        </div>
+      </div>
     </>
   );
 };
